4dcff08a860213ca6caa411c7ec292bc6726ca32
김우재 <kwj1270@naver.com>
2021-09-12 01:17:01 +0900

    feat: init project

---
+++ src/main/kotlin/com/study/kwj1270/KotlinPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kwj1270
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/kwj1270/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.kwj1270
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
