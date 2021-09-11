d71d99ee1f67ddef51bb7ed19776076ded9c1c84
김우재 <kwj1270@naver.com>
2021-09-12 01:31:11 +0900

    feat: write information in Main Class

--- src/main/kotlin/com/study/kwj1270/Main.kt
+++ src/main/kotlin/com/study/kwj1270/Main.kt
@@ -2,6 +2,11 @@ package com.study.kwj1270
 
 class Main
 
-fun main(args: Array<String>) {
+// 함수 선언시 fun 키워드
+fun main(args: Array<String>) { // (파라미터 이름: 타입) 형태를 주로 사용
     println("Hello, world!")
 }
+// 클래스 안에 함수를 넣을 필요 없이 최상위 수준에서도 함수 정의 가능하다.
+// 배열도 일반적인 클래스이며, 자바와 달리 처리 필요 없다.(컬렉션 그냥 쓰는듯)
+// println 과 같이 유용한 메서드를 한번에 사용할 수 있는 라이브러리를 제공한다.
+// 세미 콜론을 더 이상 붙이지 않아도 된다.
